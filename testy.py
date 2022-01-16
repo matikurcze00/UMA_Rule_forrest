6dc1a298416312b652b89b2a8585bb1322728671
Łukasz Tumialis <l.tumialis@stud.elka.pw.edu.pl>
2022-01-16 22:07:12 +0100

    Update testy.py

--- testy.py
+++ testy.py
@@ -1,4 +1,6 @@
 from reguly import Zbior_Regul
+from reguly import skroc_kompleks_ogolny
+from reguly import rozszerz_kompleks
 
 sloneczna = 'sloneczna'
 pochmurna = 'pochmurna'
@@ -87,20 +89,34 @@ selektor_temperatura_14 = umiarkowana
 selektor_wilgotnosc_14 = duza
 selektor_wiatr_14 = silny
 
-kompleks_1 = [selektor_aura_1, selektor_temperatura_1, selektor_wilgotnosc_1, selektor_wiatr_1]
-kompleks_2 = [selektor_aura_2, selektor_temperatura_2, selektor_wilgotnosc_2, selektor_wiatr_2]
-kompleks_3 = [selektor_aura_3, selektor_temperatura_3, selektor_wilgotnosc_3, selektor_wiatr_3]
-kompleks_4 = [selektor_aura_4, selektor_temperatura_4, selektor_wilgotnosc_4, selektor_wiatr_4]
-kompleks_5 = [selektor_aura_5, selektor_temperatura_5, selektor_wilgotnosc_5, selektor_wiatr_5]
-kompleks_6 = [selektor_aura_6, selektor_temperatura_6, selektor_wilgotnosc_6, selektor_wiatr_6]
-kompleks_7 = [selektor_aura_7, selektor_temperatura_7, selektor_wilgotnosc_7, selektor_wiatr_7]
-kompleks_8 = [selektor_aura_8, selektor_temperatura_8, selektor_wilgotnosc_8, selektor_wiatr_8]
-kompleks_9 = [selektor_aura_9, selektor_temperatura_9, selektor_wilgotnosc_9, selektor_wiatr_9]
-kompleks_10 = [selektor_aura_10, selektor_temperatura_10, selektor_wilgotnosc_10, selektor_wiatr_10]
-kompleks_11 = [selektor_aura_11, selektor_temperatura_11, selektor_wilgotnosc_11, selektor_wiatr_11]
-kompleks_12 = [selektor_aura_12, selektor_temperatura_12, selektor_wilgotnosc_12, selektor_wiatr_12]
-kompleks_13 = [selektor_aura_13, selektor_temperatura_13, selektor_wilgotnosc_13, selektor_wiatr_13]
-kompleks_14 = [selektor_aura_14, selektor_temperatura_14, selektor_wilgotnosc_14, selektor_wiatr_14]
+# kompleks_1 = [selektor_aura_1, selektor_temperatura_1, selektor_wilgotnosc_1, selektor_wiatr_1]
+# kompleks_2 = [selektor_aura_2, selektor_temperatura_2, selektor_wilgotnosc_2, selektor_wiatr_2]
+# kompleks_3 = [selektor_aura_3, selektor_temperatura_3, selektor_wilgotnosc_3, selektor_wiatr_3]
+# kompleks_4 = [selektor_aura_4, selektor_temperatura_4, selektor_wilgotnosc_4, selektor_wiatr_4]
+# kompleks_5 = [selektor_aura_5, selektor_temperatura_5, selektor_wilgotnosc_5, selektor_wiatr_5]
+# kompleks_6 = [selektor_aura_6, selektor_temperatura_6, selektor_wilgotnosc_6, selektor_wiatr_6]
+# kompleks_7 = [selektor_aura_7, selektor_temperatura_7, selektor_wilgotnosc_7, selektor_wiatr_7]
+# kompleks_8 = [selektor_aura_8, selektor_temperatura_8, selektor_wilgotnosc_8, selektor_wiatr_8]
+# kompleks_9 = [selektor_aura_9, selektor_temperatura_9, selektor_wilgotnosc_9, selektor_wiatr_9]
+# kompleks_10 = [selektor_aura_10, selektor_temperatura_10, selektor_wilgotnosc_10, selektor_wiatr_10]
+# kompleks_11 = [selektor_aura_11, selektor_temperatura_11, selektor_wilgotnosc_11, selektor_wiatr_11]
+# kompleks_12 = [selektor_aura_12, selektor_temperatura_12, selektor_wilgotnosc_12, selektor_wiatr_12]
+# kompleks_13 = [selektor_aura_13, selektor_temperatura_13, selektor_wilgotnosc_13, selektor_wiatr_13]
+# kompleks_14 = [selektor_aura_14, selektor_temperatura_14, selektor_wilgotnosc_14, selektor_wiatr_14]
+kompleks_1 = [selektor_temperatura_1, selektor_wilgotnosc_1, selektor_wiatr_1]
+kompleks_2 = [selektor_temperatura_2, selektor_wilgotnosc_2, selektor_wiatr_2]
+kompleks_3 = [selektor_temperatura_3, selektor_wilgotnosc_3, selektor_wiatr_3]
+kompleks_4 = [selektor_temperatura_4, selektor_wilgotnosc_4, selektor_wiatr_4]
+kompleks_5 = [selektor_temperatura_5, selektor_wilgotnosc_5, selektor_wiatr_5]
+kompleks_6 = [selektor_temperatura_6, selektor_wilgotnosc_6, selektor_wiatr_6]
+kompleks_7 = [selektor_temperatura_7, selektor_wilgotnosc_7, selektor_wiatr_7]
+kompleks_8 = [selektor_temperatura_8, selektor_wilgotnosc_8, selektor_wiatr_8]
+kompleks_9 = [selektor_temperatura_9, selektor_wilgotnosc_9, selektor_wiatr_9]
+kompleks_10 = [selektor_temperatura_10, selektor_wilgotnosc_10, selektor_wiatr_10]
+kompleks_11 = [selektor_temperatura_11, selektor_wilgotnosc_11, selektor_wiatr_11]
+kompleks_12 = [selektor_temperatura_12, selektor_wilgotnosc_12, selektor_wiatr_12]
+kompleks_13 = [selektor_temperatura_13, selektor_wilgotnosc_13, selektor_wiatr_13]
+kompleks_14 = [selektor_temperatura_14, selektor_wilgotnosc_14, selektor_wiatr_14]
 przyklad_1 = kompleks_1 + [1]
 przyklad_2 = kompleks_2 + [0]
 przyklad_3 = kompleks_3 + [1]
@@ -122,6 +138,26 @@ zbior_l = [kompleks_3, kompleks_4]
 zbior_T = [przyklad_1, przyklad_2, przyklad_3, przyklad_4, przyklad_5, przyklad_6, przyklad_7, przyklad_8, przyklad_9, przyklad_10, przyklad_11, przyklad_12, przyklad_13, przyklad_14]
 zbior_P = [przyklad_1, przyklad_2, przyklad_3, przyklad_4, przyklad_5, przyklad_6, przyklad_7, przyklad_8, przyklad_9, przyklad_10, przyklad_11, przyklad_12, przyklad_13, przyklad_14]
 
-Reguly = Zbior_Regul(zbior_P, kompleks_pogoda_ogolny)
+
+print(zbior_P)
+print("--------------------------")
+Reguly = Zbior_Regul(zbior_P, kompleks_pogoda_ogolny, [1, 2, 3])
 print(Reguly.zbior_Regul)
-print(len(Reguly.zbior_Regul))
+# print(len(Reguly.zbior_Regul))
+# print(Reguly.klasyfikacja(przyklad_4))
+
+# print("--------------------------")
+# print("PRZYKLAD")
+# print(przyklad_3)
+print("--------------------------")
+print("KOMPLEKS OGOLNY")
+print(kompleks_pogoda_ogolny)
+print("--------------------------")
+print(skroc_kompleks_ogolny(kompleks_pogoda_ogolny, []))
+
+kompleks_krotszy = [[sloneczna, deszczowa], [ciepla], [normalna]]
+
+
+print("--------------------------")
+print(len(kompleks_pogoda_ogolny))
+print(rozszerz_kompleks(kompleks_krotszy, kompleks_pogoda_ogolny, [1]))
